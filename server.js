fe511b3a3eba6a8f34b3b883e38c6854355c3cd8
clausfr <claus.hays@partner.akdb.de>
2020-07-23 12:14:34 +0200

    Remove unnecessary routes

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const app = express();
 const port = 2999;
 
 app.get('/logout', (req, res) => setTimeout(() => res.redirect('/'), 2000));
-app.get('/Shibboleth-SP', (req, res) => setTimeout(() => res.redirect('/Shibboleth-IDP'), 1000));
-app.get('/Shibboleth-IDP', (req, res) => setTimeout(() => res.redirect('/'), 1000));
 app.use('/public', express.static('public'));
 app.get('/*', (req, res) => res.sendFile('public/index.html', { root: __dirname }));
 
